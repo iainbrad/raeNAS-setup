0f0702cc39690631298c25ac4fe68378b5a2f819
iainbrad <iain.bradley@hotmail.co.uk>
2020-11-14 17:43:27 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# raeNAS-setup
+
+`apt update && apt install git -y`
+
+`git clone https://github.com/iainbrad/raeNAS-setup.git`
+
+`cd raeNAS-setup`
+
+`chmod +x setup.sh`
+
+`./setup.sh`
---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# install additional packages needed and perform full upgrade of all other packages
+echo Installing packages necessary for all installations...
+apt install zip unzip -y && apt clean
+
+# tidy directory
+rm README.md
+rm ~/*.deb
+mv ~/raeNAS-setup/openvpn-as ~/
+mv ~/raeNAS-setup/noip ~/
+mv ~/raeNAS-setup/plex-server ~/
+
+# set up openvpn
+echo Setting up openVPN...
+cd ~/openvpn-as
+docker-compose up -d
+
+# set up noip
+echo Setting up noip...
+cd ~/noip
+docker-compose up -d
+
+# set up plex server stack
+echo Setting up Plex stack...
+cd ~/plex-server
+chmod +x plex-update.sh
+chmod +x plex-claim.sh
+docker-compose up -d
+docker-compose stop
+
+# copy configurations to docker volumes
+cd ~
+unzip ~/plex-server/configs.zip
+rm ~/plex-server/configs.zip
+cp -r ~/jackett_config/_data/ /var/lib/docker/volumes/plex-server_jackett_data/_data/
+cp -r ~/radarr_config/_data/ /var/lib/docker/volumes/plex-server_radarr_data/_data/
+rm -r ~/jackett_config/
+rm -r ~/radarr_config/
+
+# setup cron task to refresh all of Plex stack every Wednesday at midnight
+crontab -l 2>/dev/null; echo "0 0 * * 3 ~/plex-server/plex-update.sh > /dev/null" | crontab -
+
+# start script to claim Plex server
+cd ~/plex-server
+# ./plex-claim.sh
