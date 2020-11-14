5debb4d6452b4c403966e3c3cb0d57d557198b14
iainbrad <iain.bradley@hotmail.co.uk>
2020-11-14 17:42:53 +0000

    Add files via upload

---
+++ plex-server/plex-claim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# remove all setup scripts
+rm -r ~/raeNAS-setup
+
+clear
+echo "**********************************************"
+echo "**********************************************"
+echo "*****                                    *****"
+echo "*****         USER INPUT NEEDED          *****"
+echo "*****                                    *****"
+echo "*****  Go to https://www.plex.tv/claim/  *****"
+echo "*****    click on 'Copy to Clipboard'    *****"
+echo "*****      paste into prompt below       *****"
+echo "*****                                    *****"
+echo "**********************************************"
+echo "**********************************************"
+
+read -p 'Plex Claim Code: ' claimcode
+
+# replace PLEXCLAIM with user input
+sed -i "s/PLEXCLAIM/$claimcode/g" docker-compose.yml
+
+# start plex stack
+docker-compose up -d
