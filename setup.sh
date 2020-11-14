9bf6e705017e419591d4b5ce39e229d7a5e175bb
iainbrad <iain.bradley@hotmail.co.uk>
2020-11-14 20:21:19 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,9 +16,17 @@ mv ~/raeNAS-setup/openvpn-as ~/
 mv ~/raeNAS-setup/noip ~/
 mv ~/raeNAS-setup/plex-server ~/
 
-# set up openvpn
+# set up openvpn - run to create volume then stop
 echo Setting up openVPN...
 cd ~/openvpn-as
+docker-compose up -d
+docker-compose down
+cd ~
+unzip ~/openvpn-as/openvpn-config.zip
+rm ~/openvpn-as/openvpn-config.zip
+cp -r ~/openvpn-as_openvpn_data/_data/. /var/lib/docker/volumes/openvpn-as_openvpn_data/_data
+rm -r ~/openvpn-as_openvpn_data
+cd openvpn-as
 docker-compose up -d
 
 # set up noip
@@ -26,18 +34,18 @@ echo Setting up noip...
 cd ~/noip
 docker-compose up -d
 
-# set up plex server stack
+# set up plex server stack - run to create volume then stop
 echo Setting up Plex stack...
 cd ~/plex-server
 chmod +x plex-update.sh
 chmod +x plex-claim.sh
 docker-compose up -d
-docker-compose stop
+docker-compose down
 
 # copy configurations to docker volumes
 cd ~
-unzip ~/plex-server/configs.zip
-rm ~/plex-server/configs.zip
+unzip ~/plex-server/plexserver-config.zip
+rm ~/plex-server/plexserver-config.zip
 cp -r ~/plex-server_jackett_data/_data/. /var/lib/docker/volumes/plex-server_jackett_data/_data
 cp -r ~/plex-server_radarr_data/_data/. /var/lib/docker/volumes/plex-server_radarr_data/_data
 rm -r ~/plex-server_radarr_data/
